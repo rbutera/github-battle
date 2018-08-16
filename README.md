c1ca84109854b722452bf4c3fdf8d2b3867e1667
Rai Butera <rai@rbutera.com>
2018-08-16 12:37:04 +0100

    added index.js and css

---
+++ README.md
@@ -0,0 +1,5 @@
+# github battle
+
+Practice React.JS project built using React + Webpack
+
+Live demo here:
---
+++ app/index.js
@@ -0,0 +1,9 @@
+var React = require("react")
+var ReactDOM = require("react-dom")
+require("./index.css")
+
+class App extends React.Component {
+  render() {
+    return <div>Hello, World</div>
+  }
+}
