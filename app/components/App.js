64a301aef07f6b1ac42757dcc2cedfcbedde5970
Rai Butera <rai@rbutera.com>
2018-08-28 18:16:15 +0100

    finished adding react router

--- app/components/App.js
+++ app/components/App.js
@@ -6,6 +6,7 @@ import Nav from "./nav"
 import Home from "./Home"
 import Battle from "./Battle"
 import Popular from "./Popular"
+const Switch = ReactRouter.Switch
 
 class App extends React.Component {
   render() {
@@ -13,9 +14,12 @@ class App extends React.Component {
       <Router>
         <div className="container">
           <Nav />
-          <Route exact path="/" component={Home} />
-          <Route path="/popular" component={Popular} />
-          <Route path="/battle" component={Battle} />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/battle" component={Battle} />
+            <Route path="/popular" component={Popular} />
+            <Route render={() => <p>Not Found</p>} />
+          </Switch>
         </div>
       </Router>
     )
